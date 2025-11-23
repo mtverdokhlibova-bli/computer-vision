6fcf549c3d71dffbc98fa172384e5ab812708f4d
Masha <mtverdokhlibova@gmail.com>
2025-11-23 17:56:31 +0200

    Add files via upload

--- HW_lesson2.py
+++ HW_lesson2.py
@@ -1,13 +1,13 @@
 import cv2
 import numpy as np
-# image = cv2.imread('images/photo.jpg')
-# print(image.shape)
-# image = cv2.resize(image, (image.shape[1] // 2, image.shape[0] // 2))
-# image =  cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-# image = cv2.Canny(image, 100, 180)
-# carnal = np.ones((2, 2), np.uint8)
-# image = cv2.dilate(image, carnal, iterations=1)
-# image = cv2.erode(image, carnal, iterations=1)
+image = cv2.imread('images/photo.jpg')
+print(image.shape)
+image = cv2.resize(image, (image.shape[1] // 2, image.shape[0] // 2))
+image =  cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image = cv2.Canny(image, 100, 180)
+carnal = np.ones((2, 2), np.uint8)
+image = cv2.dilate(image, carnal, iterations=1)
+image = cv2.erode(image, carnal, iterations=1)
 
 image = cv2.imread('images/photo1.jpg')
 print(image.shape)
@@ -18,6 +18,8 @@ carnal = np.ones((5, 5), np.uint8)
 image = cv2.dilate(image, carnal, iterations=1)
 image = cv2.erode(image, carnal, iterations=1)
 
+cv2.imwrite("photo.jpg", image)
+cv2.imwrite("photo1.jpg", image)
 
 cv2.imshow('image', image)
 cv2.waitKey(0)
