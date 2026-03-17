1b0629562f959aa4b6fada95eb22b4c82bf0bb09
Masha <mtverdokhlibova@gmail.com>
2026-03-17 15:21:04 +0200

    Add files via upload

---
+++ CW_lesson_10.py
@@ -0,0 +1,55 @@
+import cv2
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+def generate_image(color, shape):
+    img = np.zeros((200,200,3), np.uint8)
+
+    if shape == "circle":
+        cv2.circle(img, (100,100), 50, color, -1)
+    elif shape == "square":
+        cv2.rectangle(img, (50,50), (150,150), color, -1)
+    elif shape == "triangle":
+        points = np.array([[100, 40], [40, 160], [160, 100]])
+        cv2.drawContours(img, [points], 0, color, -1)
+    return img
+
+X = [] #список ознак
+y = [] #список міток
+
+colors = {
+    'red': (0, 0, 255),
+    'green': (0, 255, 0),
+    'blue': (255, 0, 0),
+}
+
+shapes = ['circle','square','triangle']
+
+for color_name, bgr in colors.items():
+    for shape in shapes:
+        for _ in range(10):
+            img = generate_image(bgr, shape)
+            mean_color = cv2.mean(img)[:3] #середнє значення кольорів шукаєм
+            features = [mean_color[0], mean_color[1], mean_color[2]] #середнє значення по b, g, r
+            # числові значення по яким буде навчатись наша модель
+
+            X.append(features)
+            y.append(f'{color_name}_{shape}')
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratify=y)
+
+model = KNeighborsClassifier(n_neighbors=3)
+model.fit(X_train, y_train)
+
+accuracy = model.score(X_test, y_test)
+print(f'Точність моделі {round(accuracy * 100, 2)}%')
+test_img = generate_image((255, 15, 20), 'square')
+mean_color = cv2.mean(test_img)[:3]
+features = model.predict([mean_color])
+print(f'Передбачення: {features}')
+cv2.imshow('test', test_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
